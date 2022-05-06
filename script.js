fb5280b1be21e9fceb41ca5df29e886793d241fb
Angyalne Zsuzsa <holczerzs@gmail.com>
2022-05-06 15:09:42 +0200

    basic added

---
+++ script.js
@@ -0,0 +1,41 @@
+//select some HTML elements
+const sections = document.querySelectorAll('.section');
+const sectionBtns = document.querySelectorAll('.controls');
+const sectionBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.container');
+
+let currentBtn = document.querySelectorAll('.active-btn');
+
+function active() {
+    for (let i = 0; i < sectionBtn.length; i++) {
+        sectionBtn[i].addEventListener('click', function(){
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' avtive-btn';
+        })
+    } 
+
+    allSections.addEventListener('click', (e) => {
+        const id = e.target.dataset.id;
+        if(id) {
+            sectionBtns.forEach((btn) => {
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            sections.forEach((section) => {
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active')
+        }
+    })
+
+    const toggleBtn = document.querySelector('.toggle-btn');
+    toggleBtn.addEventListener('click', () => {
+        let item = document.body;
+        item.classList.toggle('light-mode')
+    })
+}
+
+active();
